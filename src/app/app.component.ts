e0824d5a0c1a0f110f13827612b23c7ba3d703bc
Peter Schuhknecht <info@apollu.de>
2023-01-12 09:10:32 +0100

    Erste Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shoppingcart';
+  public products = [
+    { id: 0, title: 'Produkt Eins', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing dolor', price: 145},
+    { id: 2, title: 'Produkt Drei', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr', price: 27},
+    { id: 3, title: 'Produkt Vier', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing dolor', price: 1065},
+    { id: 4, title: 'Produkt Fünf', description: 'At accusam dolor sit amet, consetetur sit dolores elitr', price: 459},
+    { id: 6, title: 'Produkt Sieben', description: 'At vero eos et accusam et justo duo dolores et ea rebum', price: 12},
+  ]
+
+  public cart = [
+    { id: 1, title: 'Produkt Zwei', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing dolor', price: 145},
+    { id: 5, title: 'Produkt Sechs', description: 'At accusam dolor sit amet, consetetur sit dolores elitr', price: 459}
+  ]
 }
