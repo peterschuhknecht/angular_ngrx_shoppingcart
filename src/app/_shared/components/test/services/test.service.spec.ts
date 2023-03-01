5edabb4800b11e35958e2ccba08f52eee175a8a6
Peter Schuhknecht <info@apollu.de>
2023-03-01 13:20:22 +0100

    Ersten Test für Mock Service geschrieben, Mock Service erstellt, Zwei Test Funktionen mit Mock Daten angelegt, handle Error Funktion erstellt um Errors zu verarbeiten

--- src/app/_shared/components/test/services/test.service.spec.ts
+++ src/app/_shared/components/test/services/test.service.spec.ts
@@ -1,16 +1,29 @@
-import { TestBed } from '@angular/core/testing';
-
 import { TestService } from './test.service';
+import { of } from "rxjs";
 
 describe('TestService', () => {
   let service: TestService;
+  let httpClientSpy: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TestService);
+    // TestBed.configureTestingModule({});
+    // service = TestBed.inject(TestService);
+    httpClientSpy = {
+      get: jest.fn()
+    }
+    service = new TestService(httpClientSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should test getDataV1 function', () => {
+    const res = "Testdata";
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    jest.spyOn(httpClientSpy, 'get').mockReturnValue(of(res));
+    service.getDataV1();
+    expect(httpClientSpy.get).toBeCalledTimes(1);
+    expect(httpClientSpy.get).toHaveBeenCalledWith(url);
   });
 });
--- src/app/_shared/components/test/services/test.service.ts
+++ src/app/_shared/components/test/services/test.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable, tap } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public getDataV1(): Observable<any> {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    return this.http.get(url);
+  }
+
+  public getDataV2(): Observable<any> {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    return this.http.get(url).pipe(
+      tap((data: any) => console.log('Data fetched', data)),
+      catchError(this.handleError('Failed to fetch data'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation') {
+    return (error: HttpErrorResponse): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error);
+
+      const message = `server returned code ${error.status} with body ${error.error}`;
+      // TODO: transforming error for ui
+      throw new Error(`${operation} failed: ${message}`);
+    }
+  }
 }
