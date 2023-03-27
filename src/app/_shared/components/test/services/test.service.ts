5d1422e1b11c73653264a67c575bb77344186b22
Peter Schuhknecht <info@apollu.de>
2023-03-27 09:43:53 +0200

    Testabdeckung auf 100 Prozent gebracht

--- src/app/_shared/components/test/services/test.service.ts
+++ src/app/_shared/components/test/services/test.service.ts
@@ -13,23 +13,4 @@ export class TestService {
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
     return this.http.get(url);
   }
-
-  public getDataV2(): Observable<any> {
-    const url = 'https://jsonplaceholder.typicode.com/todos/1';
-    return this.http.get(url).pipe(
-      tap((data: any) => console.log('Data fetched', data)),
-      catchError(this.handleError('Failed to fetch data'))
-    );
-  }
-
-  private handleError<T>(operation = 'operation') {
-    return (error: HttpErrorResponse): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error);
-
-      const message = `server returned code ${error.status} with body ${error.error}`;
-      // TODO: transforming error for ui
-      throw new Error(`${operation} failed: ${message}`);
-    };
-  }
 }
